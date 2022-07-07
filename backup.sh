b77b3696811392ed2a34db27c765d45ad800fac7
Avishek Sen <x0rzavi@gmail.com>
2022-07-08 00:18:46 +0530

    Arch stuff

--- backup.sh
+++ backup.sh
@@ -7,6 +7,7 @@ set -euo pipefail
 if [ "$UID" -eq "0" ]; then
     rsync --recursive --relative --verbose --human-readable --ignore-missing-args --files-from="$(pwd)/path_list.txt" / "$(pwd)"
     chown -hR x0rzavi:x0rzavi "$(pwd)"
+    paru -Q > "$(pwd)/home/x0rzavi/installed_pkgs"
     echo -e "\nFiles updated successfully!"
 else
     echo "Root privileges are required!"
