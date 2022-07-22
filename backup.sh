eb1d8d176ecdab89edeff1ee7e0ea8907c44c0e6
Avishek Sen <x0rzavi@gmail.com>
2022-07-22 14:28:12 +0530

    Sync with gentoo

--- backup.sh
+++ backup.sh
@@ -7,7 +7,11 @@ set -euo pipefail
 if [ "$UID" -eq "0" ]; then
     rsync --recursive --relative --verbose --human-readable --ignore-missing-args --files-from="$(pwd)/path_list.txt" / "$(pwd)"
     chown -hR x0rzavi:x0rzavi "$(pwd)"
-    paru -Q > "$(pwd)/home/x0rzavi/installed_pkgs"
+    if [[ -f /etc/pacman.conf ]]; then
+        paru -Q > "$(pwd)/home/x0rzavi/installed_pkgs_arch"
+    else
+        cp /var/lib/portage/world "$(pwd)/home/x0rzavi/installed_pkgs_gentoo"
+    fi
     echo -e "\nFiles updated successfully!"
 else
     echo "Root privileges are required!"
