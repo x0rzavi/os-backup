b5597be2e062f9ba915a528e90cc762e8f37ee56
Avishek Sen <x0rzavi@gmail.com>
2022-08-01 10:40:13 +0530

    Sync

--- backup.sh
+++ backup.sh
@@ -1,18 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/env dash
 
 # Safer script
+set -o errexit
+set -o nounset
 trap "exit" INT
-set -euo pipefail
 
-if [ "$UID" -eq "0" ]; then
+if [ "$(id -u)" -eq 0 ]
+then
     rsync --recursive --relative --verbose --human-readable --ignore-missing-args --files-from="$(pwd)/path_list.txt" / "$(pwd)"
     chown -hR x0rzavi:x0rzavi "$(pwd)"
-    if [[ -f /etc/pacman.conf ]]; then
+    if [ -f /etc/pacman.conf ]; then
         paru -Q > "$(pwd)/home/x0rzavi/installed_pkgs_arch"
     else
         cp /var/lib/portage/world "$(pwd)/home/x0rzavi/installed_pkgs_gentoo"
     fi
-    echo -e "\nFiles updated successfully!"
+    printf '%s\n' 'Files updated successfully!'
 else
-    echo "Root privileges are required!"
+    printf '%s\n' 'Root privileges are required!'
 fi
